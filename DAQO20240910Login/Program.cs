0b77e3948cded593f8b1024e7bad9dce6bdde204
Andrea7001 <qo24001@esfe.agape.edu.sv>
2024-09-10 20:34:34 -0600

    Cambio de Program.cs

--- DAQO20240910Login/Program.cs
+++ DAQO20240910Login/Program.cs
@@ -1,11 +1,37 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ReturnUrlParameter = "unauthorized";
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Response.ContentType = "application/json";
+                var message = new
+                {
+                    error = "No autorizado",
+                    message = "Debes iniciar sesión para acceder a este recurso."
+                };
+
+                var jsonMessage = JsonSerializer.Serialize(message);
+                return context.Response.WriteAsync(jsonMessage);
+
+            }
+        };
+
+    });
 
 var app = builder.Build();
 
