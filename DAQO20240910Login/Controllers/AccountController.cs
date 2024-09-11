fe38e56b2d5e709d5e539a2d1a3fcb61eb474652
Andrea7001 <qo24001@esfe.agape.edu.sv>
2024-09-10 20:39:26 -0600

    Controlador Account

---
+++ DAQO20240910Login/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAQO20240910Login.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET: api/<AccountController>
+        [HttpGet("login")]
+        public IActionResult Login(string login, string password)
+        {
+            if (login == "admin" && password == "12345")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login),
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                };
+
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), authProperties);
+
+                return Ok("Inicio Sesión correctamente.");
+            }
+            else
+            {
+                return Unauthorized("Credenciales incorrectas");
+            }
+        }
+
+        [HttpPost("logout")]
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Ok("Cerro Sesión correctamente");
+        }
+    }
+}
